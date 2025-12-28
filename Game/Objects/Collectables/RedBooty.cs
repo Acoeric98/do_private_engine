fdd17e489a8759ff6b0c715ac5ca001dd94dbd80
Acoeric98 <98672535+Acoeric98@users.noreply.github.com>
2025-12-28 02:46:04 +0100

    Adjust booty rewards

--- Game/Objects/Collectables/RedBooty.cs
+++ Game/Objects/Collectables/RedBooty.cs
@@ -1,4 +1,6 @@
 ﻿using Ow.Game.Movements;
+using Ow.Game.Objects;
+using Ow.Game.Objects.Stations;
 using Ow.Managers;
 using Ow.Net.netty.commands;
 using Ow.Utils;
@@ -16,56 +18,42 @@ namespace Ow.Game.Objects.Collectables
 
         public override void Reward(Player player)
         {
-           
-                double rand = Randoms.random.NextDouble();
+            GrantBattleStationModule(player);
 
-                rand = 0.15;
-
-                if (rand <= 0.02)
-                {
-                    double rand2 = Randoms.random.NextDouble();
-
-                    if (rand2 <= 0.5)
-                    {
-                        //test = "lf4";
-                    }
-                    else
-                    {
-                        //test = "apis";
-                    }
-                }
-                else if (rand <= 0.09)
-                {
-                    //test = "gemi tasarım varsa 10k uri";
-                }
-                else if (rand <= 0.15)
-                {
-                    var hours = Randoms.random.NextDouble() <= 0.1 ? 10 : 1;
-                    var boosterTypes = Randoms.random.NextDouble() <= 0.25 ? new int[] { 1, 16, 9, 11, 6, 3 } : new int[] { 0, 15, 8, 10, 5, 2 };
-                    var boosterType = boosterTypes[Randoms.random.Next(boosterTypes.Length)];
-
-                    player.BoosterManager.Add((BoosterType)boosterType, hours);
-                }
-                else if (rand <= 0.4)
-                {
-                    var logdisk = Randoms.random.Next(1, 10);
-                }
-                else
-                {
-                    var uridium = Randoms.random.Next(1000, 4000);
-                    player.ChangeData(DataType.URIDIUM, uridium);
-                    QueryManager.SavePlayer.Information(player);
-                }
-
-                player.Equipment.Items.BootyKeys--;
-				QueryManager.SavePlayer.Information(player);
-                player.SendPacket($"0|A|BK|{player.Equipment.Items.BootyKeys}");
-           
+            player.Equipment.Items.BootyKeys--;
+            QueryManager.SavePlayer.Information(player);
+            player.SendPacket($"0|A|BK|{player.Equipment.Items.BootyKeys}");
         }
 
         public override byte[] GetCollectableCreateCommand()
         {
             return CreateBoxCommand.write("PIRATE_BOOTY_RED", Hash, Position.Y, Position.X);
+        }
+
+        private void GrantBattleStationModule(Player player)
+        {
+            var moduleTypes = new short[]
+            {
+                StationModuleModule.HULL,
+                StationModuleModule.DEFLECTOR,
+                StationModuleModule.REPAIR,
+                StationModuleModule.LASER_HIGH_RANGE,
+                StationModuleModule.LASER_MID_RANGE,
+                StationModuleModule.LASER_LOW_RANGE,
+                StationModuleModule.ROCKET_MID_ACCURACY,
+                StationModuleModule.ROCKET_LOW_ACCURACY,
+                StationModuleModule.HONOR_BOOSTER,
+                StationModuleModule.DAMAGE_BOOSTER,
+                StationModuleModule.EXPERIENCE_BOOSTER
+            };
+
+            var selectedType = moduleTypes[Randoms.random.Next(moduleTypes.Length)];
+            var moduleId = Randoms.CreateRandomID();
+
+            player.Storage.BattleStationModules.Add(new ModuleBase(moduleId, selectedType, false));
+            QueryManager.SavePlayer.Modules(player);
+
+            player.SendPacket($"0|A|STD|Battlestation modul nyitás: {selectedType} (ID: {moduleId})");
         }
     }
 }
