631d3d8dee1bc447b6c95d2bdcf6354d6d39de49
MrPepe1 <kulipeter74@gmail.com>
2025-12-31 21:13:31 +0100

    Update Collectable.cs

--- Game/Objects/Collectable.cs
+++ Game/Objects/Collectable.cs
@@ -97,10 +97,10 @@ namespace Ow.Game.Objects
 
             {
                 p.SendPacket("0|A|STM|msg_booty-key-blue_auto_buy_not_active");
-                p.SendPacket("1|A|STM|msg_booty-key-silver_auto_buy_not_active");
-                p.SendPacket("2|A|STM|msg_booty-key-red_auto_buy_not_active");
-                p.SendPacket("3|A|STM|msg_booty-key-green_auto_buy_not_active");
-                p.SendPacket("4|A|STM|msg_booty-key-gold_auto_buy_not_active");
+                p.SendPacket("0|A|STM|msg_booty-key-silver_auto_buy_not_active");
+                p.SendPacket("0|A|STM|msg_booty-key-red_auto_buy_not_active");
+                p.SendPacket("0|A|STM|msg_booty-key-green_auto_buy_not_active");
+                p.SendPacket("0|A|STM|msg_booty-key-gold_auto_buy_not_active");
             }
             
             Character = null;
